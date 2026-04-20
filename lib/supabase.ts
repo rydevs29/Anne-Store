899127590e86036ed86ea00d2a07d305ccb3cd13
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-04-20 17:21:49 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Mengambil URL dan Key dari file .env.local
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
